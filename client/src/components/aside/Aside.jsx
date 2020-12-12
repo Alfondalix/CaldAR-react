fcc4d56cccecfbd1603f52245889126a96f33dd1
Alfonso <alfondalix@gmail.com>
2020-12-12 15:31:52 -0300

    CAL-026: Create a repository on your computer

---
+++ client/src/components/aside/Aside.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Aside.css';
+
+class Aside extends React.Component{
+
+  render(){
+    return(
+      <>
+        <div>
+          <aside className="aside-container">
+
+          <h1 className="title">CaldAR</h1>
+          </aside>
+        </div>
+      </>
+    );
+  }
+}
+
+export default Aside;
